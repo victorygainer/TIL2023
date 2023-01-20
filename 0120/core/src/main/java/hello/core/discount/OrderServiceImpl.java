6d75c96bfd16bec7148614e932e8ff10630b35de
Joe Jeong <75518521+victorygainer@users.noreply.github.com>
2023-01-20 21:08:23 +0900

    스프링 핵심 원리 - 새로운 할인 정책 적용
    스프링 핵심 원리 - 새로운 할인 정책 적용

---
+++ 0120/core/src/main/java/hello/core/discount/OrderServiceImpl.java
@@ -0,0 +1,19 @@
+package hello.core.discount;
+
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String productName, int productPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, productPrice);
+
+        return new Order(memberId, productName, productPrice, discountPrice);
+    }
+}
