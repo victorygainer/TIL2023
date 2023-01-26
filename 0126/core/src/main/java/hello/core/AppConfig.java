5e2d030f5167ba9d7fef38bcd7ad04ab98bc7ffc
Joe Jeong <75518521+victorygainer@users.noreply.github.com>
2023-01-26 21:42:24 +0900

    스프링 핵심 원리 - 새로운 구조와 할인정책 적용
    스프링 핵심 원리 - 새로운 구조와 할인정책 적용

---
+++ 0126/core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,28 @@
+package hello.core;
+
+import hello.core.discount.*;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+
+public class AppConfig {
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
+}
+
