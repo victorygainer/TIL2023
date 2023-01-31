83c19340540d59cfd703c69d1579aed2ce8c7bb5
Joe Jeong <75518521+victorygainer@users.noreply.github.com>
2023-01-31 23:05:50 +0900

    스프링 핵심 원리 - 컴포넌트 스캔
    스프링 핵심 원리 - 컴포넌트 스캔

---
+++ 0131/core/src/main/java/hello/core/discount/OrderServiceImpl.java
@@ -0,0 +1,32 @@
+package hello.core.discount;
+
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String productName, int productPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, productPrice);
+
+        return new Order(memberId, productName, productPrice, discountPrice);
+    }
+
+    //테스트 용도
+    public MemberRepository getMemberRepository(){
+        return memberRepository;
+    }
+}
